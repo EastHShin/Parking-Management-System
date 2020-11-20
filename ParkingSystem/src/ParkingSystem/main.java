b068ae6eabae785ff949aa98724b75ef7faf31e0
EastHShin <64204666+EastHShin@users.noreply.github.com>
2020-11-21 01:59:08 +0900

    add time exception

--- ParkingSystem/src/ParkingSystem/main.java
+++ ParkingSystem/src/ParkingSystem/main.java
@@ -2,8 +2,10 @@ package ParkingSystem;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 public class main {
 	private static int i = 0;
@@ -20,13 +22,19 @@ public class main {
 		int num;
 		int vehiNum;
 		
+		
 		String date;
 		loop:
 		while(true) {
 			System.out.println("원하는 기능을 선택하세요!");
 			System.out.printf("1. 입차\n2. 출차\n3. 주차차량 보기\n4. 총 수입 보기\n5. 종료\n");
+			try {
 			strNum = in.nextLine();
 			choice = Integer.parseInt(strNum);
+			}catch(NumberFormatException e) {
+				System.out.println("숫자를 입력하셔야 됩니다.");
+				continue;
+			}
 			switch(choice) {
 			case 1:			//입차
 				System.out.println("차량 종류를 입력하세요! 승용차(c), 버스(b), 트럭(t)");
@@ -60,6 +68,8 @@ public class main {
 				try {
 				SimpleDateFormat fm = new SimpleDateFormat("yyyy MM dd HH mm");
 				Date to = fm.parse(date);
+				if(!rightTime(date))
+					break; 
 				insert(vehicle, typeOfvehicle, num, vehiNum, to);
 				}catch(ParseException e) {
 					System.out.println("잘못된 날짜 입력 형식입니다!");
@@ -85,6 +95,8 @@ public class main {
 				try {
 					SimpleDateFormat form = new SimpleDateFormat("yyyy MM dd HH mm");
 					Date to = form.parse(date);
+					if(!rightTime(date))
+						break;
 					p.findVehicleNum(vehicle, vehiNum, to);
 					}catch(ParseException e) {
 						System.out.println("잘못된 날짜 입력 형식입니다!");
@@ -111,7 +123,7 @@ public class main {
 	}
 	
 	private static void insert(Vehicle[] vehicle, char type, int num, int vehiNum, Date to) {
-		
+		try {
 		if(type == 'c') {			//car
 			if(num == 0)
 				vehicle[i++] = new Car(vehiNum, to);
@@ -123,6 +135,10 @@ public class main {
 		}
 		else if(type == 't') {			//truc
 			vehicle[i++] = new Truc(num, vehiNum, to);
+		}
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("주차장이 꽉 찼습니다!");
+			return;
 		}
 	}
 	public static int getCurrentIndex() {
@@ -136,6 +152,52 @@ public class main {
 			return true;
 		else
 			return false;
+	}
+	private static boolean rightTime(String date) {
+		int exceptMonth, exceptdate, hour, minute;
+		String[] splitArr;
+		GregorianCalendar gr = new GregorianCalendar();
+		splitArr = date.split(" ");
+		exceptMonth = Integer.parseInt(splitArr[1]);
+		exceptdate = Integer.parseInt(splitArr[2]);
+		hour = Integer.parseInt(splitArr[3]);
+		minute = Integer.parseInt(splitArr[4]);
+		if(hour > 24 || minute > 59) {
+			System.out.println("시간이 올바르지 않습니다.");
+			return false;
+		}
+		switch(exceptMonth) {
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			if(exceptdate > 31) {
+				System.out.println("이 달은 31일까지 입니다.");
+				return false;
+			}
+			break;
+		case 4: case 6: case 11:
+			if(exceptdate > 30) {
+				System.out.println("이 달은 30일까지 입니다.");
+				return false;
+			}
+			break;
+		case 2:
+			if(gr.isLeapYear(exceptMonth)) {
+				if(exceptdate > 29) {
+					System.out.println("이 달은 29일까지 입니다.");
+					return false;
+				}
+			}
+			else {
+				if(exceptdate > 28) {
+					System.out.println("이 달은 28일 까지 입니다.");
+					return false;
+				}
+			}
+			break;
+		default:
+			System.out.println("1월~12월까지만 입력 가능합니다.");
+			return false;
+		}
+		return true;
 	}
 	
 }
